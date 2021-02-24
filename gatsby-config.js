a13bd3cd0b97e5554f892e8a09423a4659c73003
Agrim Sharma <76flyershd@gmail.com>
2021-02-23 20:58:04 -0500

    added the GA id through environment variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -8,7 +12,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "***REMOVED***", 
+        trackingId: process.env.GA_TRACKING_ID, 
         head: true,
         anonymize: true,
       },
